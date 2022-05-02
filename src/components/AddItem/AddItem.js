9fe4eb41fa8ae2ffbc3b8a706e900061c26caa3e
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-05-03 01:50:38 +0600

    create product and delete product

--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -2,25 +2,70 @@ import React from "react";
 import "./AddItem.css";
 
 const AddItem = () => {
+
+
+
+
+
+  const handleAddProduct = (event) => {
+    event.preventDefault();
+    const supplierName = event.target.supplierName.value;
+    const name = event.target.name.value;
+    const quantity = event.target.quantity.value;
+    const Price = event.target.Price.value;
+    const image = event.target.image.value;
+
+    const product = {
+      supplierName,
+      name,
+      quantity,
+      Price,
+      image,
+      
+    };
+
+    // send data to the server
+    fetch("http://localhost:5000/product", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(product),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("success", data);
+        alert("product add successfully!!!");
+        // toast.success("Product Add Successful", { id: "success" });
+        event.target.reset();
+      });
+  };
+
+
+
+
+
+
+
   return (
     <div className="main-div">
       <h1 className="m-5">Please Add Your Items</h1>
-      <form className="inpur-field">
+      <form onSubmit={handleAddProduct} className="inpur-field">
         <div className="input-div">
-          <input className="input" type="text" placeholder="Product Name" />
+          <input className="input" name="name" type="text" placeholder="Product Name" />
           <br />
-          <input className="input" type="text" placeholder="Supplier Name" />
+          <input className="input" name="supplierName" type="text" placeholder="Supplier Name" />
           <br />
-          <input className="input" type="text" placeholder="Image Url" />
+          <input className="input" name="image" type="text" placeholder="Image Url" />
           <br />
-          <input className="input" type="text" placeholder="Price" />
+          <input className="input" name="Price" type="text" placeholder="Price" />
           <br />
-          <input className="input" type="text" placeholder="Quantity" />
+          <input className="input" name="quantity" type="text" placeholder="Quantity" />
           <br />
           <input className="input" type="text" placeholder="Email" />
           <br />
         </div>
-          <button className="update-button">Update Product</button>
+          <button className="update-product">Update Product</button>
       </form>
     </div>
   );
--- src/components/DeleteComponent/DeleteComponent.js
+++ src/components/DeleteComponent/DeleteComponent.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './DeleteComponent.css';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./DeleteComponent.css";
 
 const DeleteComponent = ({ product }) => {
-    const { _id, name, image, Price, quantity, supplierName } = product;
-    return (
-      <div>
-        <div className="products-info">
-          <img src={image} alt="" />
-          <p className="name">{name}</p>
-          <p className="name">Supplier-{supplierName}</p>
-          <p className="price">{Price} - Taka</p>
-          <p>Quantity-{quantity}</p>
-          <button className="update-button">
-            <Link to={`/productDetails/${_id}`} className="all-products-btn">
-              Update 
-            </Link>{" "}
-          </button>
-          <button className='delete-button'>Delete</button>
-        </div>
+  console.log(product);
+  const { _id, name, image, Price, quantity, supplierName } = product;
+
+
+  const [product1, setProduct1] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/product")
+      .then((res) => res.json())
+      .then((data) => setProduct1(data));
+  }, []);
+  const handleUserDelete = (productId) => {
+    const proceed = window.confirm("Are you sure you want to delete?");
+    if (proceed) {
+      console.log("deleting user with id, ", productId);
+      const url =` http://localhost:5000/product/${productId}`;
+      fetch(url, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          const remaining = product1.filter(
+            (product) => product._id !== productId
+          );
+          setProduct1(remaining);
+        });
+    }
+  };
+
+
+  return (
+    <div>
+      <div className="products-info">
+        <img src={image} alt="" />
+        <p className="name">{name}</p>
+        <p className="name">Supplier-{supplierName}</p>
+        <p className="price">{Price} - Taka</p>
+        <p>Quantity-{quantity}</p>
+        <button className="update-button">
+          <Link to={`/productDetails/${_id}`} className="all-products-btn">
+            Update
+          </Link>{" "}
+        </button>
+        <button onClick={() => handleUserDelete(product._id)} className="delete-button">Delete</button>
       </div>
-    );
+    </div>
+  );
 };
 
 export default DeleteComponent;
